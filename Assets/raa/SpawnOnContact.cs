8f6ee9749678ecc9b1d026ccdd9a4ee61436eeb9
Rishi Astra <40644327+bobthefroggywoggy@users.noreply.github.com>
2020-03-11 09:10:03 -0400

    Added ethane

---
+++ Assets/raa/SpawnOnContact.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnOnContact : MonoBehaviour {
+	public GameObject target;
+	public DataManager dataManager;
+	public MoleculeCreator script;
+	public string[] toSpawn;
+	public int[] number;
+
+	private MoleculeData[] datas;
+	// Use this for initialization
+	void Start () {
+		datas = new MoleculeData[toSpawn.Length];
+		for (int i = 0; i < toSpawn.Length; i++)
+		{
+			datas[i] = dataManager.loadMolecule(toSpawn[i], toSpawn[i].Substring(toSpawn[i].IndexOf(".")));
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnCollisionEnter(Collision col)
+	{
+		if(target == col.gameObject)
+		{
+			Spawn();
+		}
+	}
+
+	private void Spawn()
+	{
+		int upTill = number.Length;
+		if(toSpawn.Length < upTill) upTill = toSpawn.Length;
+		for(int i = 0;i < toSpawn.Length; i++)
+		{
+			for (int j = 0; j < number[i]; j++)
+			{
+				script.instantiateMolecule(datas[i], transform.position);
+			}
+		}
+	}
+}
